e3d3bbb81f95388a85ea14cac7fb79dc408171ac
Mrudhula Dubasi <mrudhuladubasi@gmail.com>
2023-08-19 10:13:44 +0530

    Asset transfer from ETH to POLY
    Deploying an NFT collection on the Ethereum blockchain, Map the collection to Polygon, and Transfer assets over via the Polygon Bridge.

---
+++ metadata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0x6be7205AE9f1D5BF3950d6a8203a88Da9a0D8074"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "RadhaKrishna",
+    image: "QmUBVa2S2hWejnbhY2qLwHcXX5tp1ceGeft35eRDBtgEgk",
+  },
+  {
+    name: "1",
+    description: "RadhaKrishna",
+    image: "QmUBVa2S2hWejnbhY2qLwHcXX5tp1ceGeft35eRDBtgEgk",
+  }, //change
+  {
+    name: "2",
+    description: "RadhaKrishna",
+    image: "QmUBVa2S2hWejnbhY2qLwHcXX5tp1ceGeft35eRDBtgEgk",
+  },
+  {
+    name: "3",
+    description: "RadhaKrishna",
+    image: "QmUBVa2S2hWejnbhY2qLwHcXX5tp1ceGeft35eRDBtgEgk",
+  },
+  {
+    name: "4",
+    description: "RadhaKrishna.",
+    image: "QmUBVa2S2hWejnbhY2qLwHcXX5tp1ceGeft35eRDBtgEgk",
+  },
+];
+
+module.exports = nfts;
